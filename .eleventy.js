6cef6afb477e729a2cb01ff97d0bf3df0c932458
TimothyOD <kompilasion@gmail.com>
2020-06-08 17:00:36 +0700

    date time

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,10 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('images')
     eleventyConfig.addPassthroughCopy('admin')
 
+    const {
+        DateTime
+    } = require("luxon");
+
     // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
     eleventyConfig.addFilter('htmlDateString', (dateObj) => {
         return DateTime.fromJSDate(dateObj, {
