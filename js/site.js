38eeb347dde7e2fae5395f128a8ae35fc8455849
TimothyOD <kompilasion@gmail.com>
2020-06-12 17:47:39 +0700

    Temp Landing

--- js/site.js
+++ js/site.js
@@ -7,3 +7,11 @@ $(function() {
         }
     });
 });
+
+
+
+const logo = document.querySelectorAll("#ynk-home path");
+
+for (let i = 0; i < logo.length; i++) {
+    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+}
