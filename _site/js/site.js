fb722840fa4a1a25fafbb5ac23745da9dcc681ec
TimothyOD <kompilasion@gmail.com>
2020-06-14 00:20:32 +0700

    Hanburger Redo

--- _site/js/site.js
+++ _site/js/site.js
@@ -7,3 +7,13 @@ $(function() {
         }
     });
 });
+
+// show burger menu
+function myFunction() {
+    var x = document.getElementById('ynk-burger-nav');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
