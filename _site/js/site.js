d12132acf2bf9478e6194f315aa6ae3db76b7736
TimothyOD <kompilasion@gmail.com>
2020-06-21 00:03:59 +0700

    Nav & Media Qs Redo

--- _site/js/site.js
+++ _site/js/site.js
@@ -9,11 +9,22 @@ $(function() {
 });
 
 // show burger menu
-function myFunction() {
-    var x = document.getElementById('ynk-burger-nav');
-    if (x.style.display === 'block') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
-}
+// function myFunction() {
+//     var x = document.getElementById('ynk-burger-nav');
+//     if (x.style.display === 'block') {
+//         x.style.display = 'none';
+//     } else {
+//         x.style.display = 'block';
+//     }
+// }
+
+const navSlide = () => {
+    const burger = document.querySelector('.ynk-burger');
+    const nav = document.querySelector('.ynk-nav');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('toggle');
+    });
+};
+
+navSlide();
